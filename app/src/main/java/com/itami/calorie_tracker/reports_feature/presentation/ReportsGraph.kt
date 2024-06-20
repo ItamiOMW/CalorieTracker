fd119042231e05ad2bb5cebc852ff2c17028b07a
Вадим <itamiomw@gmail.com>
2024-06-20 14:13:41 +0300

    1) Nav animations for bottom bar nav graphs;

--- app/src/main/java/com/itami/calorie_tracker/reports_feature/presentation/ReportsGraph.kt
+++ app/src/main/java/com/itami/calorie_tracker/reports_feature/presentation/ReportsGraph.kt
@@ -1,5 +1,9 @@
 package com.itami.calorie_tracker.reports_feature.presentation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
@@ -16,7 +20,31 @@ fun NavGraphBuilder.reportsGraph(
         route = Graph.Reports.route,
         startDestination = ReportsGraphScreen.Reports.fullRoute
     ) {
-        composable(route = ReportsGraphScreen.Reports.fullRoute) {
+        composable(
+            route = ReportsGraphScreen.Reports.fullRoute,
+            enterTransition = {
+                when (this.initialState.destination.parent?.route) {
+                    Graph.Diary.route -> {
+                        slideIntoContainer(
+                            towards = AnimatedContentTransitionScope.SlideDirection.Left,
+                        ) + fadeIn(animationSpec = tween(200))
+                    }
+                    else -> {
+                        fadeIn(animationSpec = tween(200))
+                    }
+                }
+            },
+            exitTransition = {
+                if (this.targetState.destination.parent?.route == Graph.Diary.route) {
+                    slideOutOfContainer(
+                        towards = AnimatedContentTransitionScope.SlideDirection.Right,
+                        animationSpec = tween(200)
+                    )
+                } else {
+                    fadeOut(tween(200))
+                }
+            }
+        ) {
             ReportsScreen(
                 onShowSnackbar = onShowSnackbar,
                 onNavigateToProfile = {
